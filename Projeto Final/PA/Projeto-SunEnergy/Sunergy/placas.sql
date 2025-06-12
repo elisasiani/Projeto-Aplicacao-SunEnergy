efd88490a6707326294d2bcd51b453a05c054244
Eliane Maria <146597452+ElianeOSilva@users.noreply.github.com>
2025-06-11 22:39:12 -0300

    Add files via upload

---
+++ Projeto Final/PA/Projeto-SunEnergy/Sunergy/placas.sql
@@ -0,0 +1,15 @@
+-- Active: 1748714702988@@localhost@3306@mysql
+CREATE DATABASE placas;
+
+USE placas;
+
+CREATE TABLE IF NOT EXISTS placas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(50) NOT NULL,
+    modelo VARCHAR(100) NOT NULL,
+    fabricante VARCHAR(100) NOT NULL,
+    potencia FLOAT NOT NULL,
+    tensao FLOAT NOT NULL,
+    corrente FLOAT NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+);
